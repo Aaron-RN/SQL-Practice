3a96c52446039f7e41081c60ae7ba5f5a6be1516
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2019-10-22 12:52:07 -0400

    Update 01_Select_Basics.sql

--- 01_Select_Basics.sql
+++ 01_Select_Basics.sql
@@ -6,7 +6,7 @@ Note that strings (pieces of text that are data) should be in 'single quotes';
 
 Modify it to show the population of Germany*/
 SELECT population FROM world
-  WHERE name = 'Germany'
+  WHERE name = 'Germany';
   
 /*2.Checking a list The word IN allows us to check if an item is in a list.
 The example shows the name and population for the countries 'Brazil', 'Russia', 'India' and 'China'.
@@ -19,4 +19,4 @@ SELECT name, population FROM world
 The example below shows countries with an area of 250,000-300,000 sq. km.
 Modify it to show the country and the area for countries with an area between 200,000 and 250,000.*/
 SELECT name, area FROM world
-  WHERE area BETWEEN 200000 AND 250000
+  WHERE area BETWEEN 200000 AND 250000;
